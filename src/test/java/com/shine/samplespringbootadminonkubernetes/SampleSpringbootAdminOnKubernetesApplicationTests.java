8174fe48d64a99cb5a5d06eaffdd9f8d5c3e016b
qianyu03 <qianyu03@corp.netease.com>
2023-03-31 17:40:35 +0800

    add kubernetes config

---
+++ src/test/java/com/shine/samplespringbootadminonkubernetes/SampleSpringbootAdminOnKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shine.samplespringbootadminonkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootAdminOnKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
